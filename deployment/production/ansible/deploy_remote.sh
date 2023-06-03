73cb15942f412579d16c750012675951be6e2375
cahyafaisalreza <cf.reza07@gmail.com>
2023-06-04 00:22:30 +0700

    edit

--- deployment/production/ansible/deploy_remote.sh
+++ deployment/production/ansible/deploy_remote.sh
@@ -3,4 +3,4 @@ echo "=== Deploying project to production server ==="
 date
 
 cd "$(dirname "$0")"
-ansible-playbook deploy.yml --start-at-task='Django migrate' -i hosts/remote
+ansible-playbook deploy.yml -i hosts/remote
--- myproject/settings/production.py
+++ myproject/settings/production.py
@@ -2,5 +2,5 @@ from ._base import *
 
 DEBUG = False
 ALLOWED_HOSTS = [
-    "*",
+    "*", "localhost",
 ]
